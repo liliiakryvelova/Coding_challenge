629a5c02c1cc665a462544b74415b84f15331897
liliia87 <lilia.krivelyova@gmail.com>
2022-10-08 22:59:28 -0700

    Insertion sort

--- problem#5.js
+++ problem#5.js
@@ -49,10 +49,22 @@ const mergeSort = (arr) => {
     }
 
   }
-
-
   return [...sortedArr, ...left, ...right];
+}
+
+function insertionSort(arr){
+  for(let i=0; i<arr.length; i++){
+    let currentValue = arr[i];
+
+    let j;
+    for(j=i-1; j>=0 && arr[j]>currentValue; j-- ){
+      arr[j+1] = arr[j];
+    }
+    arr[j+1] = currentValue;
+  }
+  return arr;
 }
 
 console.log(quickSort(arr));
 console.log(mergeSort(arr));
+console.log(insertionSort(arr));
