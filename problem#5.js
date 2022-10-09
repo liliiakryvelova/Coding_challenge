261d2efc35b7cd39645f422f136f1148d4eb8979
liliia87 <lilia.krivelyova@gmail.com>
2022-10-08 23:07:29 -0700

    Selection sort

--- problem#5.js
+++ problem#5.js
@@ -79,7 +79,22 @@ const bubbleSort = (arr) => {
   return arr;
 }
 
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }     
+    }
+    [array[i], array[minIndex]] = [array[minIndex], array[i]];
+  }
+  return array;
+}
+
 console.log(quickSort(arr));
 console.log(mergeSort(arr));
 console.log(insertionSort(arr));
 console.log(bubbleSort(arr));
+console.log(selectionSort(arr));
